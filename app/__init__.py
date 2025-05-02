e8e56bb147f4d0cbdcaf6d2142fbb423df354ae8
Priyanshu-Das3 <priyanshuom2003@gmail.com>
2025-05-02 15:54:31 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,10 @@
+class PestDetectionSystem:
+    def __init__(self):
+        self.model = joblib.load('models/pest_detection_model_2.pkl')
+        self.scope = ['https://spreadsheets.google.com/feeds',
+                      'https://www.googleapis.com/auth/drive']
+        self.creds = ServiceAccountCredentials.from_json_keyfile_dict(
+            json.loads(os.getenv('GOOGLE_CREDS')), self.scope)
+        self.client = gspread.authorize(self.creds)
+        self._init_sheet()
 
