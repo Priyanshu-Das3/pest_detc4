a4ae308aee1b2e4861d77be8104663883b8561d1
Priyanshu-Das3 <priyanshuom2003@gmail.com>
2025-05-02 15:44:44 +0530

    Update pest_detection.py

--- app/pest_detection.py
+++ app/pest_detection.py
@@ -2,6 +2,7 @@ import gspread
 import joblib
 import os
 import numpy as np
+import json
 from oauth2client.service_account import ServiceAccountCredentials
 from datetime import datetime
 
@@ -10,8 +11,8 @@ class PestDetectionSystem:
         self.model = joblib.load('models/pest_detection_model_2.pkl')
         self.scope = ['https://spreadsheets.google.com/feeds',
                      'https://www.googleapis.com/auth/drive']
-        self.creds = ServiceAccountCredentials.from_json_keyfile_dict(
-            eval(os.getenv('GOOGLE_CREDS')), self.scope)
+       self.creds = ServiceAccountCredentials.from_json_keyfile_dict(
+    json.loads(os.getenv('GOOGLE_CREDS')), self.scope)
         self.client = gspread.authorize(self.creds)
         self._init_sheet()
     
