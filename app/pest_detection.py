70d6f4dc24b6cc2754160c0ffdfb4517d1befa77
Priyanshu-Das3 <priyanshuom2003@gmail.com>
2025-05-02 15:54:17 +0530

    Update pest_detection.py

--- app/pest_detection.py
+++ app/pest_detection.py
@@ -11,8 +11,9 @@ class PestDetectionSystem:
         self.model = joblib.load('models/pest_detection_model_2.pkl')
         self.scope = ['https://spreadsheets.google.com/feeds',
                      'https://www.googleapis.com/auth/drive']
-       self.creds = ServiceAccountCredentials.from_json_keyfile_dict(
+      self.creds = ServiceAccountCredentials.from_json_keyfile_dict(
     json.loads(os.getenv('GOOGLE_CREDS')), self.scope)
+
         self.client = gspread.authorize(self.creds)
         self._init_sheet()
     
